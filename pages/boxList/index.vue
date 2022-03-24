670fe0344099a70dfa68919928603d3567d9a2c0
Lê Công Phương Nam <namlcp@rikkeisoft.com>
2022-03-24 09:44:07 +0700

    update feature/box-list

--- pages/boxList/index.vue
+++ pages/boxList/index.vue
@@ -1,5 +1,5 @@
 <template lang="pug">
-.box-page-container.p-2
+.box-page-container.mx-3.mt-3.flex.flex-column
   .grid.justify-content-between
     .col-fixed
       h1.font-bold.m-0.font-size-4xlarge.line-height-1 Box list
@@ -55,49 +55,47 @@
             div.pt-2.pl-3.pb-1
               span.text-600.font-size-small To
             Calendar.w-full.mb-1(v-model="dateTo" :showIcon="true" inputClass="border-0" placeholder="Select")
-  .grid
-    .col
-      DataTable.w-full.table--header(v-if="boxData" :value="boxData" responsiveLayout="scroll" :selection.sync="selectedBoxes" dataKey="id"
-        :resizableColumns="true" :paginator="true" :rows="100")
-        template(#empty)
-          .flex.justify-content-between.align-items-center
-            .m-auto
-              img(:src='require("~/assets/icons/empty-table.svg")')
+  .grid.grid-nogutter.flex-1.relative.overflow-hidden
+    .col.h-full.absolute.top-0.left-0.right-0
+      DataTable.w-full.airtag-datatable.h-full.flex.flex-column(v-if="boxData" :value="boxData" responsiveLayout="scroll" :selection.sync="selectedBoxes" 
+      dataKey="id" :resizableColumns="true" :rows="20" :scrollable="false")
         Column(selectionMode="multiple" :styless="{width: '3rem'}" :exportable="false")
         Column(field="no" header="NO" sortable)
           template(#body="slotProps")
             span.font-semibold {{slotProps.index +1}} 
         Column(field="code" header="CODE" sortable bodyClass="font-semibold")
-        Column(field="sender" header="SENDER EMAIL" sortable)
-        Column(field="size" header="SIZE(CM)" sortable bodyClass="font-semibold")
-        Column(field="weight" header="WEIGHT(KG)" sortable bodyClass="font-semibold")
-        Column(field="warehouse" header="WAREHOUSE" sortable)
+        Column(field="sender" header="SENDER EMAIL" sortable className="w-2")
+        Column(field="size" header="SIZE(CM)" sortable className="p-text-right" bodyClass="font-semibold")
+        Column(field="weight" header="WEIGHT(KG)" sortable className="p-text-right" bodyClass="font-semibold")
+        Column(field="warehouse" header="WAREHOUSE" sortable className="p-text-right")
           template(#body="slotProps")
-            .flex.align-items-center.cursor-pointer
+            .flex.align-items-center.cursor-pointer.justify-content-end
               span.text-primary.font-bold.font-size-small {{slotProps.data.warehouse}}
               .icon--small.icon-arrow-up-right.bg-primary
-        Column(field="location" header="LOCATION" sortable)
+        Column(field="location" header="LOCATION" sortable className="p-text-right")
           template(#body="slotProps")
-            .flex.align-items-center.cursor-pointer
+            .flex.align-items-center.cursor-pointer.justify-content-end
               span.text-primary.font-bold.font-size-small {{slotProps.data.location}}
               .icon--small.icon-arrow-up-right.bg-primary
-        Column(field="status" header="STATUS" sortable)
+        Column(field="status" header="STATUS" sortable className="p-text-right")
           template(#body="slotProps")
             Tag.px-2.bg-green-100(severity="success" :value="slotProps.data.status.toUpperCase()")
               span.font-bold.text-green-400.font-size-small {{slotProps.data.status.toUpperCase()}}
-        Column(:exportable="false" header="ACTION" :styles="{'min-width':'8rem'}" sortable)
+        Column(:exportable="false" header="ACTION" sortable className="p-text-right")
           template(#body="slotProps")
             Button.border-0.p-0.h-2rem.w-2rem.justify-content-center.surface-200
               .icon--small.icon-edit
             Button.border-0.p-0.ml-1.h-2rem.w-2rem.justify-content-center.surface-200
               .icon--small.icon-delete
-        template(#paginatorstart)
-          .flex.align-items-center(v-if="selectedBoxes.length <= 0")
-            .icon--large.icon-footer-paginator.surface-400
-            span.ml-3.text-400.font-size-small Showing 01 - 100 of 1280
-          Button(v-if="selectedBoxes.length>0").p-button-danger.opacity-70
-            .icon--small.icon-delete.bg-white
-            span.ml-3 Delete {{selectedBoxes.length}} items selected
+        template(#footer)
+          div
+            .flex.align-items-center(v-if="selectedBoxes.length <= 0")
+              .icon--large.icon-footer-paginator.surface-400
+              span.ml-3.text-400.font-size-small Showing 01 - 100 of 1280
+            Button(v-if="selectedBoxes.length>0").p-button-danger.opacity-70
+              .icon--small.icon-delete.bg-white
+              span.ml-3 Delete {{selectedBoxes.length}} items selected
+          Paginator(:rows="20" :totalRecords="totalItemsCount").p-0
 </template>
 
 <script lang="ts">
@@ -112,6 +110,7 @@ class BoxList extends Vue {
   isFilter = false
   dateFrom = null
   dateTo = null
+  totalItemsCount = 50
   warehouse = [
     { name: 'New York', code: 'NY' },
     { name: 'Rome', code: 'RM' },
@@ -152,6 +151,7 @@ body
   background: #E8EAEF
   // overflow: hidden
 .box-page-container
+  height: calc(100vh - 1rem)
   .pi-calendar:before
     content: url('~/assets/icons/calendar.svg')
   .p-calendar-w-btn
