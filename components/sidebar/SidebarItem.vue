50cae61db01f5d320bbcd2eb432b9f211f5ad0e3
Pham Tuan Binh <binhpt@hybrid-technologies.co.jp>
2022-04-05 08:18:16 +0700

    update sidebar

--- components/sidebar/SidebarItem.vue
+++ components/sidebar/SidebarItem.vue
@@ -1,18 +1,21 @@
 <template lang="pug">
   .menu-item.font-bold.flex-row(:class="{ 'active': active }")
-    router-link
-    <router-link :to="to" class="link">
-      <div class="item-icon">
-        <div v-if="!!icon" class="icon icon--large" :class="icon"></div>
-      </div>
-      <transition name="fade">
-        <div v-if="!collapsed" class="item-label">
-          <slot />
-          <span class="icon toggle icon-chevron-down" :class="{ 'surface-500': !hasChild }"></span>
-        </div>
-      </transition>
-    </router-link>
-  </div>
+    router-link(v-if="!!to" :to="to")
+      SidebarItemValue(:icon="icon", :active="active", :hasChild="hasChild")
+    div(v-else)
+      SidebarItemValue(:icon="icon", :active="active", :hasChild="hasChild")
+  //  <router-link :to="to" class="link">
+  //    <div class="item-icon">
+  //      <div v-if="!!icon" class="icon icon--large" :class="icon"></div>
+  //    </div>
+  //    <transition name="fade">
+  //      <div v-if="!collapsed" class="item-label">
+  //        <slot />
+  //        <span class="icon toggle icon-chevron-down" :class="{ 'surface-500': !hasChild }"></span>
+  //      </div>
+  //    </transition>
+  //  </router-link>
+  //</div>
 </template>
 
 <script lang='ts'>
@@ -32,7 +35,7 @@ class SidebarItem extends Vue {
 
   @Prop(Boolean) readonly active!: boolean | undefined
 
-  @Prop(Number) readonly level!: number | undefined
+  // @Prop(Number) readonly level!: number | undefined
 
   @Prop(Boolean) readonly hasChild: boolean = false
 }
@@ -41,29 +44,28 @@ export default SidebarItem
 </script>
 
 <style lang="sass" scoped>
-.menu-item
-  &:hover, &.active
-    border-radius: 4px
-    background-color: $text-color-300
+//.menu-item
+//  &:hover, &.active
+//    border-radius: 4px
+//    background-color: $text-color-300
+//
+//  .link
+//    @include flex-center-vert
+//    height: 100%
+//    color: $text-color-base
+//    font-size: $font-size-medium
+//    font-weight: $font-weight-bold
+//    cursor: pointer
+//    position: relative
+//    user-select: none
+//    text-decoration: none
+//  .item-label
+//    @include flex-center-space-between
+//    width: 100%
+//  .item-icon
+//    padding: $space-size-16
+//
+//  .toggle
+//    margin-right: $space-size-20
 
-  .link
-    @include flex-center-vert
-    height: 100%
-    color: $text-color-base
-    font-size: $font-size-medium
-    font-weight: $font-weight-bold
-    cursor: pointer
-    position: relative
-    user-select: none
-    text-decoration: none
-  .item-label
-    @include flex-center-space-between
-    width: 100%
-  .item-icon
-    padding: $space-size-16
-
-  .toggle
-    margin-right: $space-size-20
-
-.rotate
 </style>
--- components/sidebar/SidebarItemValue.vue
+++ components/sidebar/SidebarItemValue.vue
@@ -1,15 +1,22 @@
 <template lang="pug">
-  <div>
-    <div class="item-icon">
-      <div v-if="!!icon" class="icon icon--large" :class="icon"></div>
-    </div>
-    <transition name="fade">
-      <div v-if="!collapsed" class="item-label">
-        <slot />
-        <span class="icon toggle icon-chevron-down" :class="{ 'surface-500': !hasChild }"></span>
-      </div>
-    </transition>
-  </div>
+  .item-value(:class="{ 'active': active }")
+    .item__icon(v-if="!!icon")
+      .icon.icon--large(:class="icon")
+    transition(name="fade")
+      .item__label(v-if="!collapsed")
+        slot
+        span.icon.toggle.icon-chevron-down(:class="{ 'surface-500': !hasChild }")
+  //<div>
+  //  <div class="item-icon">
+  //    <div v-if="!!icon" class="icon icon--large" :class="icon"></div>
+  //  </div>
+  //  <transition name="fade">
+  //    <div v-if="!collapsed" class="item-label">
+  //      <slot />
+  //      <span class="icon toggle icon-chevron-down" :class="{ 'surface-500': !hasChild }"></span>
+  //    </div>
+  //  </transition>
+  //</div>
 </template>
 
 <script lang='ts'>
@@ -19,45 +26,42 @@ const nsSidebar = namespace('layout/store-sidebar')
 
 @Component
 class SidebarItem extends Vue {
-
+  // -- [ Statement Properties ] ----------------------------------------------------------
   @nsSidebar.State('collapsed')
   collapsed!: boolean
-
-  @Prop(String) readonly to!:   string | undefined
 
   @Prop(String) readonly icon: string | undefined
 
   @Prop(Boolean) readonly active!: boolean | undefined
 
+  @Prop(Boolean) readonly hasChild: boolean
 }
 
 export default SidebarItem
 </script>
 
 <style lang="sass" scoped>
-.menu-item
+.item-value
+  @include flex-center-vert
+  height: 100%
+  color: $text-color-base
+  font-size: $font-size-medium
+  font-weight: $font-weight-bold
+  cursor: pointer
+  position: relative
+  user-select: none
+  text-decoration: none
+
   &:hover, &.active
     border-radius: 4px
     background-color: $text-color-300
 
-  .link
-    @include flex-center-vert
-    height: 100%
-    color: $text-color-base
-    font-size: $font-size-medium
-    font-weight: $font-weight-bold
-    cursor: pointer
-    position: relative
-    user-select: none
-    text-decoration: none
-  .item-label
+  .item__label
     @include flex-center-space-between
     width: 100%
-  .item-icon
+  .item__icon
     padding: $space-size-16
 
   .toggle
     margin-right: $space-size-20
-
-.rotate
 </style>
