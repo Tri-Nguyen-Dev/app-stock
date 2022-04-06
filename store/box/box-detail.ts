e4bac6b62caea07b758660f8f87b3656f675a6c3
linhpt5 <linhpt5@rikkeisoft.com>
2022-04-06 10:06:39 +0700

    update store/box

--- store/box/box-detail.ts
+++ store/box/box-detail.ts
@@ -1,5 +1,4 @@
 import { Module, Mutation, VuexModule, Action } from 'vuex-module-decorators'
-import { Box } from '~/models/Box'
 import { $api, PathBind } from '~/utils'
 @Module({
   stateFactory: true,
@@ -14,17 +13,18 @@ export default class StoreBox extends VuexModule {
   }
 
   public stockList?: any = []
-  public boxList?: Box.Model[] = []
-  public totalBoxRecords?: number = 0
+  public boxDetail?: {} = {}
+  public totalStockRecords?: number = 0
 
   @Mutation
   setBoxList(data: any) {
-    this.boxList = data
+    this.boxDetail = data
   }
 
   @Mutation
   setStockList(data: any) {
     this.stockList = data.items
+    this.totalStockRecords = data.total
   }
 
   @Action({ commit: 'setStockList', rawError: true })
@@ -38,7 +38,7 @@ export default class StoreBox extends VuexModule {
   async actGetBoxItem(params?: any): Promise<string | undefined> {
     const url = PathBind.transform(this.context, StoreBox.STATE_URL.GET_BOX, params)
     const response: any = await $api.get(url, {params})
-    return response
+    return response[0]
   }
 
 
