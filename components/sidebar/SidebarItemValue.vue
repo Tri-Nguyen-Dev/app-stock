b785ef71838a5fd49fc1aada9557cf95b46c2762
Hoang Ngoc Hung <hunghn@rikkeisoft.com>
2022-04-08 10:13:23 +0700

    css side bar

--- components/sidebar/SidebarItemValue.vue
+++ components/sidebar/SidebarItemValue.vue
@@ -3,7 +3,9 @@
     .item__icon(v-if="!!item.icon")
       .icon.icon--large(:class="`icon-${item.icon} ${iconMenuCssClasses}`")
     transition(name="fade")
-      .item__label(v-if="!collapsed" :class="{ 'pl-16': !!item.parentId }")
+      .item__label(v-if="!collapsed" :class="{ 'pl-16': !!item.parentId, 'last-item': item.isLast }")
+        div.item__children(v-if="item.parentId")
+        div.item__rect(v-if="item.parentId")
         span {{ item.label }}
         span.icon.toggle.icon-chevron-down.surface-500(:class="iconSelectCssClasses")
 </template>
@@ -20,6 +22,8 @@ class SidebarItem extends Vue {
   collapsed!: boolean
 
   @Prop() readonly item!: any | undefined
+  @Prop() readonly parentItem!: any | undefined
+  @Prop() readonly index!: any | undefined
   @InjectReactive() readonly selectedItem!: any
 
   get active() {
@@ -29,7 +33,7 @@ class SidebarItem extends Vue {
   get iconMenuCssClasses() {
     return this.active ? 'bg-primary' : ''
   }
-
+  
   get iconSelectCssClasses() {
     let clazz = !this.item.parentId ? '' : '-rotate-90'
     if (this.active && !!this.item.parentId) {
@@ -39,21 +43,60 @@ class SidebarItem extends Vue {
     }
     return clazz
   }
+
+  get lastItem() {
+    if(this.parentItem) {
+      return this.index === this.parentItem.length -1 
+    }
+  }
 }
 
 export default SidebarItem
 </script>
 
 <style lang="sass" scoped>
+
 .child-item
   margin-left: 40px
+  &::before 
+    display: none
+  .item__label 
+    position: relative
+    &.last-item 
+      .item__children 
+        height: calc(50% - 10px)
+    .item__children 
+      position: absolute
+      top: 0
+      left: -20px
+      height: 100%
+      width: 2px
+      background-color: $bg-body-base
+    .item__rect 
+      position: absolute
+      left: -20px
+      top: 4px
+      width: 20px
+      height: 50%
+      border-bottom: 2px solid $bg-body-base
+      border-left: 2px solid $bg-body-base
+      border-bottom-left-radius: 16px
 .item-value
   @include flex-center-vert
   height: 100%
+  position: relative
   color: $text-color-base
   font-size: $font-size-medium
   font-weight: $font-weight-bold
-
+  &.active 
+   &::before 
+    content:""
+    position: absolute
+    border-radius: 0px 5px 5px 0px
+    left:-16px
+    width: 6px
+    height:35px
+    background-color: $primary
   &:hover, &.active
     border-radius: 4px
     background-color: $text-color-300
