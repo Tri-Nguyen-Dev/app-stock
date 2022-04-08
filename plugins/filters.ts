a52c654739e687e04305aa7b1e58f1a15cab981e
BinhPT2 <binhpt2@rikkeisoft.com>
2022-04-08 16:06:37 +0700

    minor fix

--- plugins/filters.ts
+++ plugins/filters.ts
@@ -1,12 +1,14 @@
 import Vue from 'vue'
 const dayjs = require('dayjs')
+// -- [ Variables ] ------------------------------------------------
+export const BOX_SIZE_MAP = new Map<string, string>([
+  ['BOX_SIZE_BIG', 'Big'],
+  ['BOX_SIZE_MEDIUM', 'Medium'],
+  ['BOX_SIZE_SMALL', 'Small']
+])
+
 // -- [ Convert Box size ] ------------------------------------------------
 Vue.filter('boxSize', (value) => {
-  const BOX_SIZE_MAP = new Map<string, string>([
-    ['BOX_SIZE_BIG', 'Big'],
-    ['BOX_SIZE_MEDIUM', 'Medium'],
-    ['BOX_SIZE_SMALL', 'Small']
-  ])
   return BOX_SIZE_MAP.get(value) || ''
 })
 
