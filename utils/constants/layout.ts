4b971b987dc2a291984b66efce0fe57906e0e16b
BinhPT2 <binhpt2@rikkeisoft.com>
2022-04-12 13:41:32 +0700

    remove wrong constants code

--- utils/constants/layout.ts
+++ utils/constants/layout.ts
@@ -16,20 +16,3 @@ export const SETTING_MENU = [
   { id: 100, icon: 'notification', label: 'Notifications' },
   { id: 200, icon: 'setting', label: 'Setting' }
 ]
-
-export const ITEM_BOX_DETAIL = [
-  { id: 1, icon: 'receipt', label: 'Receipt note ID' },
-  { id: 2, icon: 'tag-user', label: 'Create ID' },
-  { id: 4, icon: 'warehouse', label: 'Warehouse' },
-  { id: 5, icon: 'location-2', label: 'Location' },
-  { id: 6, icon: 'calendar', label: 'Create Time' },
-  { id: 7, icon: 'frame', label: 'Box Items' },
-  { id: 8, icon: 'dollar-square-2', label: 'Estimated inventory Fee' },
-  { id: 9, icon: 'resize', label: 'Box Size:' }
-]
-
-export const ITEM_SELLER_INFOR = [
-  { id: 1, icon: 'sender-name', label: 'Sender' },
-  { id: 2, icon: 'sender-email', label: 'Email' },
-  { id: 3, icon: 'sender-phone', label: 'Phone number' }
-]
