2b18bea677b4b75a74fe9976662202f33ab1571d
BinhPT2 <binhpt2@rikkeisoft.com>
2022-04-14 10:25:24 +0700

    init feature stock in

---
+++ pages/stock-in/index.vue
@@ -0,0 +1,14 @@
+<template lang="pug">
+  h1.text-center WELCOME!
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'nuxt-property-decorator'
+
+@Component
+class StockIn extends Vue {}
+
+export default StockIn
+</script>
+<style lang="sass">
+</style>
