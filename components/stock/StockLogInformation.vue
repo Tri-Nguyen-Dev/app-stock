997a339bb5876055c068894a8496334d767c91d6
rk_minhpq+1-at-909411671852 <quannhm@rikkeisoft.com>
2022-04-15 11:14:12 +0700

    fix log information

--- components/stock/StockLogInformation.vue
+++ components/stock/StockLogInformation.vue
@@ -17,11 +17,11 @@
                 div.surface-200.p-2.border-round
                   .icon--large.icon-transferring.bg-green-400(v-show='item.description === "REQUEST_ACTION_TO_INVENTORY"')
                   .icon--large.icon-outgoing.bg-orange-400(v-show='item.description === "REQUEST_ACTION_TO_EXPORT_BOX"')
-                  .icon--large.icon-incoming.bg-blue-500(v-show='item.description === "REQUEST_TO_IMPORT_BOX"')
+                  .icon--large.icon-incoming.bg-blue-500(v-show='item.description === "REQUEST_ACTION_TO_IMPORT_BOX"')
               .col
                 div.font-semibold.text-lg(v-show='item.description === "REQUEST_ACTION_TO_INVENTORY"') Transferring quantity
                 div.font-semibold.text-lg(v-show='item.description === "REQUEST_ACTION_TO_EXPORT_BOX"') Outgoing Quantity
-                div.font-semibold.text-lg(v-show='item.description === "REQUEST_TO_IMPORT_BOX"') Incoming Quantity
+                div.font-semibold.text-lg(v-show='item.description === "REQUEST_ACTION_TO_IMPORT_BOX"') Incoming Quantity
                 span.font-bold.text-lg {{item.amount}}
           .col.border-right-1.border-black-alpha-10
             .grid
