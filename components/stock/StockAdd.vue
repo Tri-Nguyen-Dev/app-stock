7ff34bc7995ab3ef844a8966f770d0819da36769
rk_minhpq+1-at-909411671852 <quannhm@rikkeisoft.com>
2022-04-21 09:21:05 +0700

    add imagePath key

--- components/stock/StockAdd.vue
+++ components/stock/StockAdd.vue
@@ -108,7 +108,7 @@ class AddNewStock extends Vue {
     length: null,
     width: null,
     height: null,
-    imageUrl: ''
+    imagePath: ''
   }
 
   extension!: string
@@ -176,6 +176,8 @@ class AddNewStock extends Vue {
         headers: {
           'Content-Type': file.files[0].type
         }
+      }) .then(() => {
+        this.stockInformation.imagePath = this.generateUploadUrl.key
       })
     })
   }
