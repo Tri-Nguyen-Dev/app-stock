85510999ccc1ed427a866b9bd645bf8cec9354db
Lê Công Phương Nam <namlcp@rikkeisoft.com>
2022-04-21 14:45:30 +0700

    update label

--- pages/stock-in/index.vue
+++ pages/stock-in/index.vue
@@ -1,30 +1,12 @@
 <template lang="pug">
-div
   h1.text-center WELCOME!
-  InputText(type="text" placeholder="Search" v-model="id")
-  InputText(type="text" placeholder="Search" v-model="boxId")
-  Button(@click='isShowLabel = true') Show
-  PrintLabel(:displayLable='isShowLabel' @setShow="setShowLabel" :requestId="id" :boxId="boxId")
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'nuxt-property-decorator'
-import PrintLabel from '~/components/pdf/PrintLabel.vue'
 
-@Component({
-  components: {
-    PrintLabel
-  }
-})
-class StockIn extends Vue {
-  isShowLabel: boolean = false;
-  id = 'RN000000000001'
-  boxId = 'psuh1w1r60hcozh3uof77fv0'
-
-  setShowLabel(value: any) {
-    this.isShowLabel = value
-  }
-}
+@Component
+class StockIn extends Vue {}
 
 export default StockIn
 </script>
