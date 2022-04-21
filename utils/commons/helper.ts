ebb7a0831721932063dbec952b1104658d108ff6
KienNC <kiennc@rikkeisoft.com>
2022-04-21 16:08:36 +0700

    fix bug ultil

--- utils/commons/helper.ts
+++ utils/commons/helper.ts
@@ -3,3 +3,8 @@ export function refreshAllFilter(filter: any) {
   for (const items in filter) filter[items] = null
   return filter
 }
+
+export function validateEmail(email) {
+  const re = /^(([^<>()[\]\\.,;:\s@\\"]+(\.[^<>()[\]\\.,;:\s@\\"]+)*)|(\\".+\\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+  return re.test(email)
+}
