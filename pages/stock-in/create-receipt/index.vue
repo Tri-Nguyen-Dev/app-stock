4ac1d0f89538e898ddd638c41cf76548a6622fe8
KienNC <kiennc@rikkeisoft.com>
2022-04-25 18:38:22 +0700

    no message

--- pages/stock-in/create-receipt/index.vue
+++ pages/stock-in/create-receipt/index.vue
@@ -1,167 +1,215 @@
 <template lang="pug">
 .receipt-note
-		card.mb-5
-				template(#title='')
-					div.d-flex
-						i.pi.pi-info-circle.mr-3
-						span.font-semibold.text-base GENERAL INFORMATION
-				template(#content='')
-					div.grid
-						.col
-							.filter__item.item--disabled
-								.filter__title ID receipt note
-								.filter__text 030133333
-						.col
-							.filter__item.item--disabled
-								.filter__title ID Creator
-								.filter__text NVN030133
-						.col
-							.filter__item.item--disabled
-								.filter__title Creator name
-								.filter__text Nguyen Khanh Hung
-						.col
-							.filter__item.item--disabled
-								.filter__title Create time
-								.filter__text 19-09-2022 9:24AM
-						.col
-							.filter__item
-								.filter__title Warehouse
-								Dropdown.general__dropdown(v-model="warehouse" :options="warehouseList" optionLabel="name" placeholder="Select")
-						.col
-							.filter__item
-								.filter__title Seller email
-								.filter__autocomplete
-									AutoComplete(v-model="seller" :suggestions="sellerList" @complete="handleChangeSeller($event)" field="email" placeholder="Enter seller email")
-								span {{ sellerEmailError }}
-						.col
-							.filter__item.item--disabled
-								.filter__title Seller phone
-								.filter__text {{ sellerPhone }}
-						.col
-							.filter__item.item--disabled
-								.filter__title Seller name
-								.filter__text {{ sellerName }}
-		card.card-custom
-			template(#content='')
-				.grid
-					.col-2.border-top.pl-3
-						.grid.mb-2
-							.col.flex.align-items-center.justify-content-center.mb-1.pt-4
-								Button.text-primary(type='button' icon='pi pi-plus' style='width: 90%; background-color: #F1F3FF; border: none' @click='addBox' label='Add box')
-						.overflow-y-auto(style='height: 55vh', v-if='listBox')
-							.grid.box-card.m-2(
-								v-for='box in listBox',
-								@click='selectBox(box)'
-								:class='{ "box-card-active": box.index == activeIndex }'
-							)
-								.col-6.flex.align-items-center
-									.icon--large.icon-box-view.bg-blue-700
-								.col-6.flex.align-items-center.justify-content-end
-									Button.p-button-default.p-button-rounded.p-button-text(
-										type='button',
-										icon='pi pi-ellipsis-h',
-										@click.stop='activeIndex=box.index;isModalDelete = true; deleteBox()'
-									)
-								.col-12.flex.align-items-center
-									.grid
-										.col-12.pb-0
-											span.uppercase.font-semibold.mr-1 box {{ box.index + 1 }}
-										.col-12.pb-0(v-if='box.location.name!=""')
-											AutoComplete.edit-location(
-												v-model="listBox[activeIndex].location"
-												field='name' 
-												:suggestions='locationList' 
-												@complete="searchLocation($event)"
-												:dropdown="true"
-											)
-												template(#item="slotProps")
-													.grid.align-items-center.grid-nogutter
-														span.font-bold.text-small {{ slotProps.item.name }}
-														.icon-arrow-up-right.icon
-					.col-10
-						.grid.border__grid(v-if ='boxSizeList && listBox[activeIndex]')
-							.d-flex.col-12.border__right(class='md:col-5 lg:col-4')
-								span.font-semibold.text-base.mr-3.ml-3 Size
-								Dropdown.box-input(
-									style='width: 70%',
-									:options='boxSizeList',
-									optionLabel='name',
-									optionValue='id',
-									placeholder='Select size',
-									v-model='listBox[activeIndex].boxSize.id'
-								)
-								span.font-semibold.text-base.ml-3 (cm)
-							.d-flex.col-12.border__right.pt-4.pb-4(class='md:col-5 lg:col-4')
-								span.font-semibold.text-base.mr-3.ml-2 Estimate Inventory Fee
-								InputNumber.number-input(
-									v-model='listBox[activeIndex].inventoryFee'
-									mode="currency" currency="USD" locale="en-US"
-								)
-								span.font-semibold.text-base.ml-3 /day
-							.d-flex.col-6(class='md:col-5 lg:col-4')
-								span.font-semibold.text-base.mr-2.ml-2 Barcode
-								InputText.box-input.mr-2(placeholder='Enter barcode' style='width:40%' @change='changeBarcode($event)' v-model='boxQrCode')
-						.grid.border__left.border__right.mt-0.pb-3(
-							style='margin-right: 0px',
-							v-if='listBox && listBox[activeIndex]'
+	card.mb-5
+		template(#title='')
+			.d-flex
+				i.pi.pi-info-circle.mr-3
+				span.font-semibold.text-base GENERAL INFORMATION
+		template(#content='')
+			.grid
+				.col
+					.filter__item.item--disabled
+						.filter__title ID receipt note
+						.filter__text 030133333
+				.col
+					.filter__item.item--disabled
+						.filter__title ID Creator
+						.filter__text NVN030133
+				.col
+					.filter__item.item--disabled
+						.filter__title Creator name
+						.filter__text Nguyen Khanh Hung
+				.col
+					.filter__item.item--disabled
+						.filter__title Create time
+						.filter__text 19-09-2022 9:24AM
+				.col
+					.filter__item
+						.filter__title Warehouse
+						Dropdown.general__dropdown(
+							v-model='warehouse',
+							:options='warehouseList',
+							optionLabel='name',
+							placeholder='Select'
 						)
-							ItemDataTable(:listItemInBox='listBox[activeIndex].listItemInBox')
-			template(#footer='')
-				.grid(v-if='listBox && listBox[activeIndex]')
-					.d-flex.pt-2(class='col-12 md:col-4 lg:col-4')
-						.grid.w-full
-							//- .col.align-items-center.ml-4
-							.col-2.flex.align-items-center.justify-content-center.pl-4
-								img(src='~/assets/icons/note.svg')
-							.col-10
-								span.font-semibold.text-base.mr-1 Note:
-								br
-								InputText.pt-0.pl-0(placeholder='Write something...' style='border:none')
-					.d-flex(class='col-6 md:col-2 lg:col-2')
-						.grid.w-full.border__right
-							.col-3.flex.align-items-center.justify-content-end
-								img(src='~/assets/icons/box-border.svg')
-							.col-9
-								span.font-semibold.text-base.mr-1 Total boxs:
-								br
-								span.font-semibold.text-primary  {{listBox.length}}
-					.d-flex(class='col-6 md:col-2 lg:col-2')
-						.grid.w-full.border__right
-							.col-3.flex.align-items-center.justify-content-end
-								img(src='~/assets/icons/total-items-border.svg')
-							.col-9
-								span.font-semibold.text-base.mr-1 Total items:
-								br
-								span.font-semibold.text-primary {{totalItem()}}
-					.d-flex(class='col-6 md:col-2 lg:col-2')
-						.grid.w-full.border__right
-							.col-3.flex.align-items-center.justify-content-end
-								img(src='~/assets/icons/total-fee.svg')
-							.col-9
-								span.font-semibold.text-base.mr-1 Total fee:
-								br
-								span.font-semibold.text-primary {{totalFee()}} $/day
-					.d-flex.justify-content-center(class='col-6 md:col-2 lg:col-2')
-						Button.p-button-secondary.mr-2(label='Save draft' icon="pi pi-file-o" @click='saveReceipt(0)')
-						Button.p-button-secondary.mr-2(label='Back' @click='clearLocation()' v-if='activeSave')
-						Button(label='Next' @click='getLocationSuggest()' v-if='!activeSave' :disabled='!activeAction')
-						Button(label='Save' @click='saveReceipt(1)' v-if='activeSave && activeAction')
-		Sidebar(
-			:visible='isShowModalAddStock',
-			:baseZIndex='1000',
-			position='right',
-			ariaCloseLabel='to'
+				.col
+					.filter__item
+						.filter__title Seller email
+						.filter__autocomplete
+							AutoComplete(
+								v-model='seller',
+								:suggestions='sellerList',
+								@complete='handleChangeSeller($event)',
+								field='email',
+								placeholder='Enter seller email'
+							)
+						span {{ sellerEmailError }}
+				.col
+					.filter__item.item--disabled
+						.filter__title Seller phone
+						.filter__text {{ sellerPhone }}
+				.col
+					.filter__item.item--disabled
+						.filter__title Seller name
+						.filter__text {{ sellerName }}
+	card.card-custom
+		template(#content='')
+			.grid
+				.col-2.border-top.pl-3
+					.grid.mb-2
+						.col.flex.align-items-center.justify-content-center.mb-1.pt-4
+							Button.text-primary(
+								type='button',
+								icon='pi pi-plus',
+								style='width: 90%; background-color: #f1f3ff; border: none',
+								@click='addBox',
+								label='Add box'
+							)
+					.overflow-y-auto(style='height: 55vh', v-if='listBox')
+						.grid.box-card.m-2(
+							v-for='box in listBox',
+							@click='selectBox(box)',
+							:class='{ "box-card-active": box.index == activeIndex }'
+						)
+							.col-6.flex.align-items-center
+								.icon--large.icon-box-view.bg-blue-700
+							.col-6.flex.align-items-center.justify-content-end
+								Button.p-button-default.p-button-rounded.p-button-text(
+									type='button',
+									icon='pi pi-ellipsis-h',
+									@click.stop='activeIndex = box.index; isModalDelete = true; deleteBox()'
+								)
+							.col-12.flex.align-items-center
+								.grid
+									.col-12.pb-0
+										span.uppercase.font-semibold.mr-1 box {{ box.index + 1 }}
+									.col-12.pb-0(v-if='box.location.name != ""')
+										AutoComplete.edit-location(
+											v-model='listBox[activeIndex].location',
+											field='name',
+											:suggestions='locationList',
+											@complete='searchLocation($event)',
+											:dropdown='true'
+										)
+											template(#item='slotProps')
+												.grid.align-items-center.grid-nogutter
+													span.font-bold.text-small {{ slotProps.item.name }}
+													.icon-arrow-up-right.icon
+				.col-10
+					.grid.border__grid(v-if='boxSizeList && listBox[activeIndex]')
+						.d-flex.col-12.border__right(class='md:col-5 lg:col-4')
+							span.font-semibold.text-base.mr-3.ml-3 Size
+							Dropdown.box-input(
+								style='width: 70%',
+								:options='boxSizeList',
+								optionLabel='name',
+								optionValue='id',
+								placeholder='Select size',
+								v-model='listBox[activeIndex].boxSize.id'
+							)
+							span.font-semibold.text-base.ml-3 (cm)
+						.d-flex.col-12.border__right.pt-4.pb-4(class='md:col-5 lg:col-4')
+							span.font-semibold.text-base.mr-3.ml-2 Estimate Inventory Fee
+							InputNumber.number-input(
+								v-model='listBox[activeIndex].inventoryFee',
+								mode='currency',
+								currency='USD',
+								locale='en-US'
+							)
+							span.font-semibold.text-base.ml-3 /day
+						.d-flex.col-6(class='md:col-5 lg:col-4')
+							span.font-semibold.text-base.mr-2.ml-2 Barcode
+							InputText.box-input.mr-2(
+								placeholder='Enter barcode',
+								style='width: 40%',
+								@change='changeBarcode($event)',
+								v-model='boxQrCode'
+							)
+					.grid.border__left.border__right.mt-0.pb-3(
+						style='margin-right: 0px',
+						v-if='listBox && listBox[activeIndex]'
+					)
+						ItemDataTable(:listItemInBox='listBox[activeIndex].listItemInBox')
+		template(#footer='')
+			.grid(v-if='listBox && listBox[activeIndex]')
+				.d-flex.pt-2.col-12(class='md:col-4 lg:col-4')
+					.grid.w-full
+						//- .col.align-items-center.ml-4
+						.col-2.flex.align-items-center.justify-content-center.pl-4
+							img(src='~/assets/icons/note.svg')
+						.col-10
+							span.font-semibold.text-base.mr-1 Note:
+							br
+							InputText.pt-0.pl-0(
+								placeholder='Write something...',
+								style='border: none'
+							)
+				.d-flex.col-6(class='md:col-2 lg:col-2')
+					.grid.w-full.border__right
+						.col-3.flex.align-items-center.justify-content-end
+							img(src='~/assets/icons/box-border.svg')
+						.col-9
+							span.font-semibold.text-base.mr-1 Total boxs:
+							br
+							span.font-semibold.text-primary {{ listBox.length }}
+				.d-flex.col-6(class='md:col-2 lg:col-2')
+					.grid.w-full.border__right
+						.col-3.flex.align-items-center.justify-content-end
+							img(src='~/assets/icons/total-items-border.svg')
+						.col-9
+							span.font-semibold.text-base.mr-1 Total items:
+							br
+							span.font-semibold.text-primary {{ totalItem() }}
+				.d-flex.col-6(class='md:col-2 lg:col-2')
+					.grid.w-full.border__right
+						.col-3.flex.align-items-center.justify-content-end
+							img(src='~/assets/icons/total-fee.svg')
+						.col-9
+							span.font-semibold.text-base.mr-1 Total fee:
+							br
+							span.font-semibold.text-primary {{ totalFee() }} $/day
+				.d-flex.justify-content-center.col-6(class='md:col-2 lg:col-2')
+					Button.p-button-secondary.mr-2(
+						label='Save draft',
+						icon='pi pi-file-o',
+						@click='saveReceipt(0)'
+					)
+					Button.p-button-secondary.mr-2(
+						label='Back',
+						@click='clearLocation()',
+						v-if='activeSave'
+					)
+					Button(
+						label='Next',
+						@click='getLocationSuggest()',
+						v-if='!activeSave',
+						:disabled='!activeAction'
+					)
+					Button(
+						label='Save',
+						@click='saveReceipt(1)',
+						v-if='activeSave && activeAction'
+					)
+	Sidebar(
+		:visible='isShowModalAddStock',
+		:baseZIndex='1000',
+		position='right',
+		ariaCloseLabel='to'
+	)
+		StockAdd(
+			@cancelAddStock='cancelAddStock',
+			@addItem='addItem',
+			:barcode='boxQrCode'
 		)
-			StockAdd(@cancelAddStock='cancelAddStock' @addItem='addItem' :barcode='boxQrCode')
-			FormAddSeller(:isShowForm="isShowFormAddSeller")
-		//- ConfirmDialogCustom(
-		//- 	title="Confirm delete box"
-		//- 	message="Are you sure you want to delete box in this receipt?"
-		//- 	image="confirm-delete"
-		//- 	:isShow="isModalDelete"
-		//- 	:onOk="deleteBox()"
-		//- 	:onCancel="handleCancel"
-		//- 	)
+		FormAddSeller(:isShowForm='isShowFormAddSeller')
+	//- ConfirmDialogCustom(
+	//- 	title="Confirm delete box"
+	//- 	message="Are you sure you want to delete box in this receipt?"
+	//- 	image="confirm-delete"
+	//- 	:isShow="isModalDelete"
+	//- 	:onOk="deleteBox()"
+	//- 	:onCancel="handleCancel"
+	//- 	)
 </template>
 <script lang="ts">
 import { Component, namespace, Vue } from 'nuxt-property-decorator'
@@ -186,7 +234,6 @@ const nsStoreLocationList = namespace('location/location-list')
   }
 })
 class CreateReceipt extends Vue {
-
   boxSize: number = 0
   listBox: ReceiptModel.Box[] = [new ReceiptModel.Box()]
   isShowModalAddStock: boolean = false
@@ -221,11 +268,11 @@ class CreateReceipt extends Vue {
   isShowFormAddSeller: boolean = false
 
   get sellerName() {
-    return (this.seller && this.seller.name) ? this.seller.name : 'name'
+    return this.seller && this.seller.name ? this.seller.name : 'name'
   }
 
   get sellerPhone() {
-    return (this.seller && this.seller.phone) ? this.seller.phone : 'phone'
+    return this.seller && this.seller.phone ? this.seller.phone : 'phone'
   }
 
   @nsStoreStock.State
@@ -263,8 +310,8 @@ class CreateReceipt extends Vue {
 
   addBox() {
     if (!this.checkActiveAction()) return
-    const item: ReceiptModel.Box = new ReceiptModel.Box()	
-    item.index= this.listBox[this.listBox.length - 1].index + 1
+    const item: ReceiptModel.Box = new ReceiptModel.Box()
+    item.index = this.listBox[this.listBox.length - 1].index + 1
     if (this.listBox.length < 10) {
       this.listBox.push(item)
     }
@@ -275,7 +322,7 @@ class CreateReceipt extends Vue {
   deleteBox() {
     if (this.listBox.length > 1) {
       this.listBox.splice(this.activeIndex, 1)
-      this.selectBox(this.listBox[this.listBox.length-1])
+      this.selectBox(this.listBox[this.listBox.length - 1])
     }
   }
 
@@ -349,7 +396,10 @@ class CreateReceipt extends Vue {
     const receiptDraft: ReceiptModel.CreateReceiptDraft =
       new ReceiptModel.CreateReceiptDraft()
     receiptDraft.action = RECEIPT_ACTION.REQUEST_ACTION_TO_IMPORT_BOX
-    receiptDraft.status = type === 0 ? RECEIPT_STATUS.REQUEST_STATUS_DRAFT : RECEIPT_STATUS.REQUEST_STATUS_SAVED
+    receiptDraft.status =
+      type === 0
+        ? RECEIPT_STATUS.REQUEST_STATUS_DRAFT
+        : RECEIPT_STATUS.REQUEST_STATUS_SAVED
     this.listBox.forEach((element) => {
       const box: ReceiptModel.BoxDraft = new ReceiptModel.BoxDraft()
       box.inventoryFee = element.inventoryFee
@@ -373,18 +423,18 @@ class CreateReceipt extends Vue {
 
   checkActiveAction() {
     this.activeAction = true
-    this.listBox.forEach(element => {
+    this.listBox.forEach((element) => {
       this.activeAction = element.listItemInBox.length > 0
     })
     return this.activeAction
   }
 
   async getLocationSuggest() {
-    const listBoxSize = this.listBox.map(element => {
+    const listBoxSize = this.listBox.map((element) => {
       return '' + element.boxSize
     })
     await this.actLocationSuggestion(listBoxSize)
-    this.boxLocation.forEach(element => {
+    this.boxLocation.forEach((element) => {
       this.listBox[element.index].location.name = element.name
       this.listBox[element.index].location.id = element.id
     })
@@ -397,7 +447,7 @@ class CreateReceipt extends Vue {
   }
 
   checkLocation() {
-    this.listBox.forEach(element => {
+    this.listBox.forEach((element) => {
       this.activeSave = !(!element.location.id || element.location.id === '')
     })
   }
@@ -415,7 +465,7 @@ class CreateReceipt extends Vue {
 
   totalItem() {
     let total = 0
-    this.listBox.forEach(element => {
+    this.listBox.forEach((element) => {
       total += element.listItemInBox.length
     })
     return total
@@ -423,8 +473,8 @@ class CreateReceipt extends Vue {
 
   totalFee() {
     let totalFee = 0
-    this.listBox.forEach(element => {
-      if(element.inventoryFee! >0) {
+    this.listBox.forEach((element) => {
+      if (element.inventoryFee! > 0) {
         totalFee += element.inventoryFee!
       }
     })
@@ -433,9 +483,11 @@ class CreateReceipt extends Vue {
 
   async changeBarcode(event) {
     if (event.target.value.length === 13) {
-      const item = this.listBox[this.activeIndex].listItemInBox.findIndex(element => {
-        return element.stock.barCode === event.target.value
-      })
+      const item = this.listBox[this.activeIndex].listItemInBox.findIndex(
+        (element) => {
+          return element.stock.barCode === event.target.value
+        }
+      )
       if (item >= 0) {
         this.listBox[this.activeIndex].listItemInBox[item].amount++
         this.boxQrCode = ''
@@ -472,7 +524,7 @@ class CreateReceipt extends Vue {
   // 		location: e.query
   // 	})
   // }, 400)
-  async	searchLocation(e){
+  async searchLocation(e) {
     await this.actLocationList({
       location: e.query
     })
