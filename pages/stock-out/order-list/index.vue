6c0d06692d9081a91aed7fd78506e52eaabc2a7d
BinhPT2 <binhpt2@rikkeisoft.com>
2022-04-27 15:16:19 +0700

    init stock out

---
+++ pages/stock-out/order-list/index.vue
@@ -0,0 +1,14 @@
+<template lang="pug">
+  h1.text-center WELCOME!
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'nuxt-property-decorator'
+
+@Component
+class DeliveryOrderList extends Vue {}
+
+export default DeliveryOrderList
+</script>
+<style lang="sass" scoped>
+</style>
