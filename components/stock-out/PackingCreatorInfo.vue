ba712d87a1d401d5420bf5356f3ce4e0cf54329f
QuanNHM <quannhm@rikkeisoft.com>
2022-04-27 11:23:30 +0700

    create packing detail page

---
+++ components/stock-out/PackingCreatorInfo.vue
@@ -0,0 +1,23 @@
+<template lang="pug">
+.packingWarehouseInfo.grid
+  .icon.icon-warehouse-info.mr-2.bg-blue-700.inline-block
+  span.uppercase.font-bold.mb-4 warehouse information
+  .col-12(className='lg:col-12 md:col-12 sm:col-12 py-3 px-2')
+    StockUnit(title="Id creator" value="BDSM6969" icon="icon-sender-name")
+  .col-12(className='lg:col-12 md:col-12 sm:col-12 py-3 px-2')
+    StockUnit(title="Email Address" value="linhndm@gmail.com" icon="icon-sender-email")
+  .col-12(className='lg:col-12 md:col-12 sm:col-12 py-3 px-2')
+    StockUnit(title="Phone number" value="+84 333 666 888" icon="icon-sender-phone")
+</template>
+<script lang="ts">
+import { Component, Vue } from 'nuxt-property-decorator'
+
+@Component
+class PackingCreatorInfo extends Vue {
+
+}
+
+export default PackingCreatorInfo
+</script>
+<style lang="sass" scoped>
+</style>
---
+++ pages/stock-out/packing-detail.vue
@@ -0,0 +1,29 @@
+<template lang="pug">
+.grid.grid-nogutter.packing__detail--container
+  .col-3.surface-0.border-round.h-full.overflow-y-auto.sub-tab
+    StockOutPackingInformationDetail
+  .col-9.pl-5.py-0.h-full.overflow-y-auto.overflow-x-hidden.flex-1
+    .grid
+      .col
+        h1.text-heading Packing detail
+        span.text-subheading 1111 products found
+      .col.flex.justify-content-end
+        .btn.btn-primary.w-2 Next
+</template>
+<script lang="ts">
+import { Component, Vue } from 'nuxt-property-decorator'
+@Component
+class PackingDetail extends Vue {
+
+}
+
+export default PackingDetail
+</script>
+<style lang="sass" scoped>
+.packing__detail--container
+  height: calc(100vh - 64px)
+  .sub-tab
+    height: calc(100vh - 32px)
+    max-width: 21.5rem
+    overflow: hidden
+</style>
