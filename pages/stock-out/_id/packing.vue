af109a385dc38b70e818fe4a4abb4cd2fd6c79d4
linhpt5 <linhpt5@rikkeisoft.com>
2022-05-09 14:19:46 +0700

    merge feature/stock-out

---
+++ pages/stock-out/_id/packing.vue
@@ -0,0 +1,14 @@
+<template lang="pug">
+  h1.text-center WELCOME!
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'nuxt-property-decorator'
+
+@Component
+class DeliveryOrderPacking extends Vue {}
+
+export default DeliveryOrderPacking
+</script>
+<style lang="sass" scoped>
+</style>
