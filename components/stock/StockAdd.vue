b8389b92128a1a08a3961d8c0e53a0fc76b7319f
KienNC <kiennc@rikkeisoft.com>
2022-05-11 14:53:01 +0700

    fix create receipt

--- components/stock/StockAdd.vue
+++ components/stock/StockAdd.vue
@@ -1,118 +1,94 @@
 <template lang="pug">
-.addStock
-	.addStockHeader
-		h3.px-3 Add item
-	.addStockContent.p-3
-		h5.my-1.required__title Barcode
-		.p-input-icon-right.w-full
-			.icon.icon--right.icon-scan
-			InputText.w-full(v-model='stockInformation.barCode')
-		h5.mb-1 SKU
-		InputText.w-full(v-model='stockInformation.sku')
-		h5.mb-1.required__title Name
-		InputText.w-full(
-			v-model='stockInformation.name',
-			:class='{ "name--error": $v.stockInformation.name.$error }'
-		)
-		.error-message(
-			v-if='$v.stockInformation.name.$dirty && !$v.stockInformation.name.required'
-		) Name cannot be empty!
-		h5.mb-1.required__title Category
-		Dropdown.w-full(
-			v-model='stockInformation.category',
-			:options='warehouseList',
-			optionLabel='name',
-			:filter='true',
-			placeholder='Select a category',
-			:showClear='true',
-			:class='{ "category--error": $v.stockInformation.category.$error }'
-		)
-			template(#value='slotProps')
-		.error-message(
-			v-if='$v.stockInformation.category.$dirty && !$v.stockInformation.category.required'
-		) Please choose category!
-		.grid
-			.col-6
-				h5.mb-1.required__title Quantity
-				InputText.w-full(
-					type='number',
-					:min='1',
-					v-model='stockInformation.quantity',
-					:class='{ "quantity--error": $v.stockInformation.quantity.$error }'
-				)
-				.error-message(
-					v-if='($v.stockInformation.quantity.$dirty && !$v.stockInformation.quantity.required) || stockInformation.quantity < 0'
-				) Please enter quantity!
-			.col-6
-				h5.mb-1.required__title Unit
-				Dropdown.w-full(
-					v-model='stockInformation.unit',
-					:options='unitList',
-					optionLabel='name',
-					:class='{ "unit--error": $v.stockInformation.unit.$error }'
-				)
-				.error-message(
-					v-if='$v.stockInformation.unit.$dirty && !$v.stockInformation.unit.required'
-				) Please choose unit!
-		.grid
-			.col-6
-				h5.mb-1 Weight
-				.p-input-icon-right.w-full
-					.icon.icon--right Kg
-					InputText.w-full(type='number', :min='1', v-model='stockInformation.weight')
-					.error-message(v-if='stockInformation.weight < 0') Weight cannot be negative!
-			.col-6
-				h5.mb-1 Value
-				.p-input-icon-right.w-full
-					.icon.icon--right $
-					InputText.w-full(type='number', :min='1', v-model='stockInformation.value')
-					.error-message(v-if='stockInformation.weight < 0') Value cannot be negative!
-		.grid.mb-3
-			.col
-				h5.mb-1 Length
-				.p-input-icon-right.w-full
-					.icon.icon--right cm
-					InputText.w-full(type='number', :min='1', v-model='stockInformation.length')
-					.error-message(v-if='stockInformation.length < 0') Length cannot be negative!
-			.col
-				h5.mb-1 Width
-				.p-input-icon-right.w-full
-					.icon.icon--right cm
-					InputText.w-full(type='number', :min='1', v-model='stockInformation.width')
-					.error-message(v-if='stockInformation.width < 0') Width cannot be negative!
-			.col
-				h5.mb-1 Height
-				.p-input-icon-right.w-full
-					.icon.icon--right cm
-					InputText.w-full(type='number', :min='1', v-model='stockInformation.height')
-					.error-message(v-if='stockInformation.height < 0') Height cannot be negative!
-		FileUpload(
-			accept='image/jpeg, image/png',
-			:fileLimit='1',
-			:showCancelButton='false',
-			:showUploadButton='false',
-			@select='getGenerateUrl'
-		)
-			template(#empty)
-				.grid
-					.col-3.align-items-center.flex
-						img(:src='require("~/assets/images/image-upload.png")')
-					.col
-						p.uppercase.text-500 image
-						p.font-bold Upload item image
-	.addStockFooter.p-3
-		.grid
-			.col
-				.text-center.surface-hover.cursor-pointer.border-round.p-1(
-					@click='cancelAddStock'
-				)
-					span.uppercase.font-semibold cancel
-			.col
-				.text-center.bg-blue-500.cursor-pointer.border-round.text-white.p-1(
-					@click='addItem'
-				)
-					span.uppercase save
-	Toast(:baseZIndex='99999')
+  .addStock
+    .addStockHeader
+      h3.px-3 Add item
+    .addStockContent.p-3
+      h5.my-1.required__title Barcode
+      .p-input-icon-right.w-full
+        .icon.icon--right.icon-scan
+        InputText(v-model='stockInformation.barCode').w-full
+      h5.mb-1 SKU
+      InputText(v-model='stockInformation.sku').w-full
+      h5.mb-1.required__title Name
+      InputText(v-model='stockInformation.name' :class="{'name--error' : $v.stockInformation.name.$error}").w-full
+      .error-message(v-if='$v.stockInformation.name.$dirty && !$v.stockInformation.name.required') Name cannot be empty!
+      h5.mb-1.required__title Category
+      Dropdown(
+        v-model='stockInformation.category'
+        :options="warehouseList" optionLabel="name"
+        :filter="true" placeholder="Select a category"
+        :showClear="true" :class="{'category--error' : $v.stockInformation.category.$error}"
+        ).w-full
+        template(#value='slotProps')
+      .error-message(v-if='$v.stockInformation.category.$dirty && !$v.stockInformation.category.required') Please choose category!
+      .grid
+        .col-6
+          h5.mb-1.required__title Quantity
+          InputText(
+            type="number"
+            :min="1" v-model='stockInformation.quantity'
+            :class="{'quantity--error' : $v.stockInformation.quantity.$error}"
+          ).w-full
+          .error-message(
+            v-if='($v.stockInformation.quantity.$dirty && !$v.stockInformation.quantity.required) || stockInformation.quantity < 0'
+          ) Please enter quantity!
+        .col-6
+          h5.mb-1.required__title Unit
+          Dropdown(
+            v-model='stockInformation.unit'
+            :options="unitList" optionLabel="name"
+            :class="{'unit--error' : $v.stockInformation.unit.$error}"
+          ).w-full
+          .error-message(v-if='$v.stockInformation.unit.$dirty && !$v.stockInformation.unit.required') Please choose unit!
+      .grid
+        .col-6
+          h5.mb-1 Weight
+          .p-input-icon-right.w-full
+            .icon.icon--right Kg
+            InputText(type="number" :min="1" v-model='stockInformation.weight').w-full
+            .error-message(v-if='stockInformation.weight < 0') Weight cannot be negative!
+        .col-6
+          h5.mb-1 Value
+          .p-input-icon-right.w-full
+            .icon.icon--right $
+            InputText(type="number" :min="1" v-model='stockInformation.value').w-full
+            .error-message(v-if='stockInformation.weight < 0') Value cannot be negative!
+      .grid.mb-3
+        .col
+          h5.mb-1 Length
+          .p-input-icon-right.w-full
+            .icon.icon--right cm
+            InputText(type="number" :min="1" v-model='stockInformation.length').w-full
+            .error-message(v-if='stockInformation.length < 0') Length cannot be negative!
+        .col
+          h5.mb-1 Width
+          .p-input-icon-right.w-full
+            .icon.icon--right cm
+            InputText(type="number" :min="1" v-model='stockInformation.width').w-full
+            .error-message(v-if='stockInformation.width < 0') Width cannot be negative!
+        .col
+          h5.mb-1 Height
+          .p-input-icon-right.w-full
+            .icon.icon--right cm
+            InputText(type="number" :min="1" v-model='stockInformation.height').w-full
+            .error-message(v-if='stockInformation.height < 0') Height cannot be negative!
+      FileUpload(accept="image/jpeg, image/png" :fileLimit="1" :showCancelButton='false' :showUploadButton='false' @select='getGenerateUrl')
+        template(#empty)
+          .grid
+            .col-3.align-items-center.flex
+              img(:src='require("~/assets/images/image-upload.png")')
+            .col
+              p.uppercase.text-500 image
+              p.font-bold Upload item image
+    .addStockFooter.p-3
+      .grid
+        .col
+          .text-center.surface-hover.cursor-pointer.border-round.p-1(@click='cancelAddStock')
+            span.uppercase.font-semibold cancel
+        .col
+          .text-center.bg-blue-500.cursor-pointer.border-round.text-white.p-1(@click='addItem')
+            span.uppercase save
+    Toast(:baseZIndex='99999')
 </template>
 <script lang="ts">
 import axios from 'axios'
@@ -159,7 +135,7 @@ class AddNewStock extends Vue {
     imagePath: ''
   }
 
-  @Prop() barcode!: string
+  @Prop() barcode!:string
   extension!: string
 
   @nsStoreWarehouse.State
