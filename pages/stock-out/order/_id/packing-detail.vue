26580ae77857e4a32768fd10bd1ba253010a8a66
rk_quannhm-at-909411671852 <quannhm@rikkeisoft.com>
2022-05-11 17:36:17 +0700

    create packing detail page

---
+++ pages/stock-out/order/_id/packing-detail.vue
@@ -0,0 +1,117 @@
+<template lang="pug">
+  .grid.grid-nogutter.packing__detail--container
+    Toast
+    .packing__detail--left.col-3.surface-0.border-round.h-full.overflow-y-auto.sub-tab
+      StockOutPackingInformationDetail(:deliveryOrderDetail="deliveryOrderDetail")
+    .col-9.ml-5.py-0.h-full.overflow-y-auto.overflow-x-hidden.flex-1.relative
+      div.flex.flex-column
+        .grid.grid-nogutter.mb-3
+          StockOutPackingOriginal(
+            title='original box'
+            icon='icon-info'
+            :isOriginal='true'
+            :listBox="listOriginalBox"
+            type='originalBox'
+          )
+        .grid.grid-nogutter.my-3
+          StockOutPackingOriginal(
+            title='outgoing box'
+            icon='icon-arrow-circle-up-right'
+            :isOutgoing='true'
+            :listBox="listOutGoingBox"
+            type='outGoingBox'
+          )
+        .grid.grid-nogutter.my-3
+          StockOutPackingOriginal(
+            title='tranferring box'
+            icon='icon-repeat'
+            :isTranffering='true'
+            :listBox="listTranfferingBox"
+            type='tranferringBox'
+          )
+      .packing__detail--footer.grid.grid-nogutter.bg-white.p-3.border-round.fixed.align-items-center.absolute.right-0.left-0.bottom-0
+        .col.p-1
+          .grid.align-items-center
+            .col-1
+              .icon--large.icon-note
+            .col
+              div Note:
+              span {{packingDetail.note || 'Note is empty'}}
+        .col-2.border-right-1.border-gray-300.p-1
+          .grid.align-items-center
+            .col-3
+              img(src='~/assets/icons/box-border.svg')
+            .col
+              span.font-semibold.text-base.mr-1 Total boxs:
+              .font-semibold.text-primary 1
+        .col-2.border-right-1.border-gray-300.p-1
+          .grid.align-items-center
+            .col-3
+              img(src='~/assets/icons/total-items-border.svg')
+            .col
+              span.font-semibold.text-base.mr-1 Total items:
+              .font-semibold.text-primary 1
+        .col-3.flex.p-1.justify-content-evenly
+          Button.p-button-outlined(label='Export file' icon="pi pi-download")
+          Button Set delivery
+</template>
+
+<script lang="ts">
+import { Component, Vue, namespace } from 'nuxt-property-decorator'
+const nsStorePackingDetail = namespace('stock-out/packing-box')
+
+@Component
+class DeliveryOrderPackingDetail extends Vue {
+  listOriginalBox: any = []
+  listOutGoingBox: any = []
+  listTranfferingBox: any = []
+
+  @nsStorePackingDetail.State('deliveryOrderDetail')
+  deliveryOrderDetail!: any
+
+  @nsStorePackingDetail.State
+  packingDetail!: any
+
+  @nsStorePackingDetail.Action
+  actGetDeliveryOrderDetail!: (id: any) => Promise<any>
+
+  @nsStorePackingDetail.Action
+  actGetPackingDetailById!: (id: any) => Promise<any>
+
+  async mounted() {
+    await Promise.all([
+      this.actGetDeliveryOrderDetail('DO000000000047'),
+      this.actGetPackingDetailById('DO000000000047')
+    ])
+
+    this.listOriginalBox = [...this.packingDetail.originalBox]
+    this.listOutGoingBox = [...this.packingDetail.outGoingBox]
+    this.listTranfferingBox = [...this.packingDetail.transferringBox]
+  }
+}
+export default DeliveryOrderPackingDetail
+</script>
+<style lang="sass" scoped>
+.packing__detail--container
+  height: calc(100vh - 32px)
+  .packing__detail--left
+    height: calc( 100% - 32px) !important
+  .sub-tab
+    height: calc(100vh - 32px)
+    max-width: 21.5rem
+    overflow: hidden
+::-webkit-scrollbar
+  width: 7px
+  height: 7px
+  background-color: #F5F5F5
+
+::-webkit-scrollbar-track
+  -webkit-box-shadow: inset 0 0 6px rgba(0,0,0,0.3)
+  border-radius: 10px
+  background-color: #F5F5F5
+
+::-webkit-scrollbar-thumb
+  border-radius: 10px
+  -webkit-box-shadow: inset 0 0 6px rgba(0,0,0,.3)
+  background-color: #979AA4
+</style>
--- store/stock-out/packing-box.ts
+++ store/stock-out/packing-box.ts
@@ -11,14 +11,15 @@ export default class StorePackingBox extends VuexModule {
   private static readonly STATE_URL = {
     GET_ORIGINAL_BOX: '/delivery-order/:id/list-original-box',
     GET_DELIVERY_ORDER: '/delivery-order/:id/detail',
-    GET_BOX_LOCATION: '/location/suggest'
+    GET_BOX_LOCATION: '/location/suggest',
+    GET_PACKING_DETAIL_BY_ID: '/delivery-order/:id/packing-detail'
   }
 
   public totalOriginalList?: number = 0
   public originalList: PackingDetail.OriginalBox[] = []
   public deliveryOrderDetail: any = {}
-
   public boxLocation: any[] = []
+  public packingDetail: any = {}
 
   @Mutation
   setListOriginal(data: any) {
@@ -34,6 +35,11 @@ export default class StorePackingBox extends VuexModule {
   @Mutation
   setLocationSuggestion(data: any) {
     this.boxLocation = data
+  }
+
+  @Mutation
+  setPackingDetail(data: any) {
+    this.packingDetail = data
   }
 
   @Action({ commit: 'setListOriginal', rawError: true })
@@ -65,5 +71,16 @@ export default class StorePackingBox extends VuexModule {
       const response = await $api.post(url, data)
       return response.data
     } catch (error) {}
+  }
+
+  @Action({ commit: 'setPackingDetail', rawError: true })
+  async actGetPackingDetailById(id: any ): Promise<any | undefined> {
+    try {
+      const url = PathBind.transform(this.context, StorePackingBox.STATE_URL.GET_PACKING_DETAIL_BY_ID, { id })
+      const response = await $api.get(url)
+      return response.data
+    } catch (error) {
+
+    }
   }
 }
