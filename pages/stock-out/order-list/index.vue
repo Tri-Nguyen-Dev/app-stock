b231936e09af4b2b2c0d59ea6dd5be9e4baccbe7
TriNV <trinv@rikkeisoft.com>
2022-05-12 08:18:07 +0700

    update

--- pages/stock-out/order-list/index.vue
+++ pages/stock-out/order-list/index.vue
@@ -418,7 +418,7 @@ class DeliveryOrderList extends Vue {
   }
 
   rowdbClick({ data }) {
-    this.$router.push(`/stock-out/order/${data.id}/packing-detail`)
+    this.$router.push(`/stock-out/order/${data.id}/packing`)
   }
 
   sortData(e: any) {
