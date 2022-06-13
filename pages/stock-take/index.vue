940d49470c41852016cebdce19c954035e37593c
TriNV <trinv@rikkeisoft.com>
2022-06-13 09:21:06 +0700

    update

--- pages/stock-take/index.vue
+++ pages/stock-take/index.vue
@@ -27,7 +27,7 @@
       div(class="col-12 lg:col-12 xl:col-4")
         .grid
           div(class="col-12 md:col-3")
-            FilterTable(title="NOTE ID" :value="filter.id" placeholder="Enter ID" name="id" :searchText="true" @updateFilter="handleFilter")
+            FilterTable(title="Note ID" :value="filter.id" placeholder="Enter ID" name="id" :searchText="true" @updateFilter="handleFilter")
           div(class="col-12 md:col-9")
             .grid.grid-nogutter
               .col
@@ -78,17 +78,97 @@
           :options="warehouseList"
           name="warehouse"
           @updateFilter="handleFilter") 
+    .grid.grid-nogutter.flex-1.relative.overflow-hidden.m-h-700
+      .col.h-full.absolute.top-0.left-0.right-0.bg-white
+        DataTable(
+          @sort="sortData($event)"
+          :class="{ 'table-wrapper-empty': !stockTakeList || stockTakeList.length <= 0 }"
+          :rowClass="rowClass" :value='stockTakeList' responsiveLayout="scroll"
+          :selection='selectedStockTake'
+          dataKey='id'
+          :rows='10'
+          :rowHover='true'
+          @row-select-all="rowSelectAll"
+          @row-unselect-all="rowUnSelectAll"
+          @row-select="rowSelect"
+          @row-unselect="rowUnselect"
+        )
+          Column(
+            selectionMode='multiple'
+            :styles="{'width': '1%'}")
+          Column(field='no' header='NO' :styles="{'width': '1%'}" )
+            template(#body='{ index }')
+              span.grid-cell-center.stock__table-no.text-white-active.text-900.font-bold {{ getIndexPaginate(index) }}
+          Column(field='imageUrl' header='Image' headerClass="grid-header-center")
+            template(#body='{ data }')
+              .stock__table__image.overflow-hidden.grid-cell-center
+                NuxtLink(:to="`/stock/${data.id}`")
+                  img.h-2rem.w-2rem.border-round(
+                    :src="data.imagePath | getThumbnailUrl" alt='' width='100%' style="object-fit: cover;")
+          Column(header='Name' field='name' :sortable="true" sortField="_name")
+            template(#body='{ data }')
+              NuxtLink.stock__table-name.text-white-active.text-base.text-900.text-overflow-ellipsis.overflow-hidden(:to="`/stock/${data.id}`" class="no-underline hover:underline") {{ data.name }}
+          Column(header='Barcode' field='barCode' :sortable="true" sortField="_barCode" headerClass="grid-header-right")
+            template(#body='{ data }')
+              .stock__table-barcode.grid-cell-right {{ data.barCode }}
+          Column(header='Category' :sortable="true" field='category' sortField="_category" headerClass="grid-header-right")
+              template(#body='{ data }')
+                div.grid-cell-right {{ data.categoryName }}
+          Column(field='status' header="Status" headerClass="grid-header-right")
+            template(#body='{ data }')
+              div.grid-cell-right
+                span.table__status.table__status--available(v-if="data.stockStatus === 'STOCK_STATUS_AVAILABLE'") Available
+                span.table__status.table__status--disable(v-if="data.stockStatus === 'STOCK_STATUS_DISABLE' ") Disable
+          Column(field='action' header="action" :styles="{'width': '2%'}")
+            template(#body='{ data }')
+              .table__action(:class="{'action-disabled': data.stockStatus === 'STOCK_STATUS_DISABLE'}")
+                span.action-item(@click.stop="handleEditStock(data.id)" :class="{'disable-button': selectedStockFilter.length > 0}")
+                  .icon.icon-edit-btn
+                span.action-item(@click.stop="showModalDelete([data])" :class="{'disable-button': selectedStockFilter.length > 0}")
+                  .icon.icon-btn-delete
+          template(#footer)
+            Pagination(
+              type="items selected"
+              :paging="paging"
+              :total="total"
+              :deleted-list="selectedStockFilter"
+              @onDelete="showModalDelete"
+              @onPage="onPage")
+          template(#empty)
+            div.table__empty
+              img(:srcset="`${require('~/assets/images/table-empty.png')} 2x`" v-if="!checkIsFilter")
+              img(:srcset="`${require('~/assets/images/table-notfound.png')} 2x`" v-else)
+              p.empty__text(v-if="!checkIsFilter") List is empty!, Click
+                span &nbsp;here
+                span(@click="handleAddStock") &nbsp;to add item.
+              p.notfound__text(v-else) Item not found!
 </template>
 <script lang="ts">
 import { Component, Vue, namespace } from 'nuxt-property-decorator'
+import {
+  PAGINATE_DEFAULT,
+  calculateIndex
+} from '~/utils'
+import Pagination from '~/components/common/Pagination.vue'
+import ConfirmDialogCustom from '~/components/dialog/ConfirmDialog.vue'
+import { Paging } from '~/models/common/Paging'
 const nsWarehouseStock = namespace('warehouse/warehouse-list')
 const nsStoreStockTake = namespace('stock-take/note-list')
 const dayjs = require('dayjs')
 
-@Component
+@Component({
+  components: {
+    ConfirmDialogCustom,
+    Pagination
+  }
+})
 class StockTake extends Vue {
+  selectedStockTake: any = []
   isShowFilter: boolean = false
   isShowOptionAddNote: boolean = false
+  isModalDelete: boolean = false
+  onEventDeleteList: any = []
+  paging: Paging.Model = { ...PAGINATE_DEFAULT, first: 0 }
   filter: any = {
     id: null,
     dateFrom: null,
@@ -105,14 +185,43 @@ class StockTake extends Vue {
   @nsStoreStockTake.State
   stockTakeList!: any
 
+  @nsStoreStockTake.State
+  total!: number
+
   @nsWarehouseStock.Action
   actWarehouseList!: () => Promise<void>
 
   @nsStoreStockTake.Action
-  actGetStockTakeList!: () => Promise<void>
+  actGetStockTakeList!: (params: any) => Promise<void>
 
   handleFilter(e: any, name: string) {
     this.filter[name] = e
+  }
+
+  // -- [ Getters ] -------------------------------------------------------------
+  get selectedStockFilter() {
+    return  _.filter(this.selectedStockTake, (stock: any) => {
+      return stock.stockStatus !== 'STOCK_STATUS_DISABLE'
+    })
+  }
+
+  async getProductList() {
+    await this.actGetStockTakeList({
+      pageSize: this.paging.pageSize,
+      pageNumber: this.paging.pageNumber,
+      ...this.getParamApi()
+    })
+  }
+
+  onPage(event: any) {
+    this.paging.pageSize = event.rows
+    this.paging.pageNumber = event.page
+    this.getProductList()
+  }
+
+  showModalDelete(data: any) {
+    this.onEventDeleteList = data || this.selectedStockFilter
+    this.isModalDelete = true
   }
 
   getParamApi() {
@@ -127,6 +236,47 @@ class StockTake extends Vue {
         'warehouseId': this.filter.warehouse?.id
       }
     }
+  }
+
+  getIndexPaginate(index: number) {
+    return calculateIndex(
+      index,
+      this.paging.pageNumber,
+      this.paging.pageSize
+    )
+  }
+
+  rowClass(data: any) {
+    return data.stockStatus === 'STOCK_STATUS_DISABLE' ? 'row-disable' : ''
+  }
+
+  rowSelectAll({ data }) {
+    this.selectedStockTake = _.union(this.selectedStockTake, data)
+  }
+
+  rowUnSelectAll() {
+    this.selectedStockTake = _.differenceWith(
+      this.selectedStockTake,
+      this.stockTakeList,
+      _.isEqual
+    )
+  }
+
+  rowSelect({ data }) {
+    this.selectedStockTake.push(data)
+  }
+
+  rowUnselect({ originalEvent, data }) {
+    originalEvent.originalEvent.stopPropagation()
+    this.selectedStockTake = _.filter(
+      this.selectedStockTake,
+      (stock: any) => stock.id !== data.id
+    )
+  }
+
+  get checkIsFilter() {
+    const params = _.omit(this.getParamApi(), ['pageNumber', 'pageSize'])
+    return Object.values(params).some((item) => item)
   }
 
   mounted() {
